5abba09a1d2844e86b50727a67967b23b8425a20
Zee <avijoy.haldar2015@gmail.com>
2020-10-06 11:13:26 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,70 @@
+var style_cookie_name = "style";
+var style_cookie_duration = 30;
+var style_domain = "";
+
+function switch_style(css_title) {
+    var i, link_tag;
+    for (i = 0, link_tag = document.getElementsByTagName("link");
+        i < link_tag.length; i++) {
+        if ((link_tag[i].rel.indexOf("stylesheet") != -1) &&
+            link_tag[i].title) {
+            link_tag[i].disabled = true;
+            if (link_tag[i].title == css_title) {
+                link_tag[i].disabled = false;
+            }
+        }
+        set_cookie(style_cookie_name, css_title,
+            style_cookie_duration, style_domain);
+    }
+}
+function set_style_from_cookie() {
+    var css_title = get_cookie(style_cookie_name);
+    if (css_title.length) {
+        switch_style(css_title);
+    }
+}
+function set_cookie(cookie_name, cookie_value,
+    lifespan_in_days, valid_domain) {
+
+    var domain_string = valid_domain ?
+        ("; domain=" + valid_domain) : '';
+    document.cookie = cookie_name +
+        "=" + encodeURIComponent(cookie_value) +
+        "; max-age=" + 60 * 60 *
+        24 * lifespan_in_days +
+        "; path=/" + domain_string;
+}
+function get_cookie(cookie_name) {
+
+    var cookie_string = document.cookie;
+    if (cookie_string.length != 0) {
+        var cookie_array = cookie_string.split('; ');
+        for (i = 0; i < cookie_array.length; i++) {
+            cookie_value = cookie_array[i].match(cookie_name + '=(.*)');
+            if (cookie_value != null) {
+                return decodeURIComponent(cookie_value[1]);
+            }
+        }
+    }
+    return '';
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function check() {
+    var togval = document.getElementById("toggle").checked;
+    if (togval == true) {
+        switch_style('light'); return false;
+    }
+    else {
+        switch_style('dark'); return false;
+    }
+}
